f6dbd6bc8191c844dbcddc3bd54b506f92d59f23
aviator42 <migu3l.r4mon@gmail.com>
2019-10-03 16:32:48 -0500

    landscape

--- landscape.py
+++ landscape.py
@@ -15,10 +15,25 @@ jim.pendown()
 jim.fillcolor("green")
 jim.begin_fill()
 
-jim.forward(300)
+jim.forward(700)
 jim.right(90)
-jim.forward(340)
+jim.forward(500)
 jim.right(90)
+jim.forward(1400)
+jim.right(90)
+jim.forward(500)
+jim.right(90)
+jim.forward(700)
+jim.end_fill()
 
+dwight.penup()
+dwight.goto(350, 400)
+dwight.pendown
+
+dwight.fillcolor("yellow")
+dwight.begin_fill()
+
+dwight.circle(45)
+dwight.end_fill()
 
 turtle.exitonclick()
